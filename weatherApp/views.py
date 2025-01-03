dc119c9dbfa938bcd42fbfa154e68f7c8adb9d6a
hoboJF <jadenfiero@gmail.com>
2025-01-02 19:35:42 -0500

    added the correct functionality to the search bar, in its current state the app is fully functional but not complete

--- weatherApp/views.py
+++ weatherApp/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 import requests
 
 def index(request):
-    API_KEY = ''
+    API_KEY = '6d9088870fb8cc3dc6d1ba210bf87c58'
     city = 'Baltimore'
+    if request.method == 'POST':
+        city = request.POST.get('city', 'Baltimore')
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={}'
     data = requests.get(url.format(city, API_KEY)).json()
     weather_data={
