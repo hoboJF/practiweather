193d9a48a2b12dec3949f3681a999d81c2d5a73c
hoboJF <jadenfiero@gmail.com>
2025-01-02 21:55:49 -0500

    added try except block to handle either nothing being inputted into search bar or a nonexistent city being input

--- weatherApp/views.py
+++ weatherApp/views.py
@@ -5,14 +5,32 @@ def index(request):
     API_KEY = ''
     city = 'Baltimore'
     if request.method == 'POST':
-        city = request.POST.get('city', 'Baltimore')
+        city = request.POST.get('city', '').strip()
+        if not city:
+            city = 'Baltimore'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={}'
-    data = requests.get(url.format(city, API_KEY)).json()
-    weather_data={
-        'city' : city,
-        'temperature' : data['main']['temp'],
-        'conditions' : data['weather'][0]['description']
-    }
+    try:
+        response = requests.get(url.format(city, API_KEY))
+        if response.status_code == 200:
+            data = response.json()
+            weather_data={
+            'city' : city,
+            'temperature' : data['main']['temp'],
+            'conditions' : data['weather'][0]['description']
+        }
+        else:
+            weather_data={
+            'city' : city,
+            'temperature' : 'N/A',
+            'conditions' : 'City is either not found or the request is invalid'
+            }
+    except requests.exceptions.RequestException as exception:
+        weather_data = {
+            'city': city,
+            'temperature': 'N/A',
+            'conditions': 'Error connecting to weather service.'
+        }
+            
     print(weather_data)
     final_data = {'weather_data' : weather_data}
     return render(request, 'weatherApp/weatherApp.html', final_data)
