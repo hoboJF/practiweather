e5281ed90fb4644aa0e9e532c9edfabc69408fe5
hoboJF <jadenfiero@gmail.com>
2025-01-03 18:06:06 -0500

    added geolocation functionality, location now defaults to wherever found location is

--- weatherApp/views.py
+++ weatherApp/views.py
@@ -2,12 +2,20 @@ from django.shortcuts import render
 import requests
 
 def index(request):
+    ip_address = requests.get('http://api.ipify.org').text
+    ip_data = requests.get(f'http://ip-api.com/json/{ip_address}').json()
+    location_data={
+        'city' : ip_data['city'],
+        'region' : ip_data['regionName'],
+        'country' : ip_data['country']
+    }
     API_KEY = ''
-    city = 'Baltimore'
+    city = f"{location_data['city']}, {location_data['region']}, {location_data['country']}"
+    print(city)
     if request.method == 'POST':
         city = request.POST.get('city', '').strip()
         if not city:
-            city = 'Baltimore'
+            city = f"{location_data['city']}, {location_data['region']}, {location_data['country']}"
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={}'
     try:
         response = requests.get(url.format(city, API_KEY))
